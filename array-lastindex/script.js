871ce9d64c89c63ac159bdfd0a04ed21199619a5
karthigasun <prkarthik3@gmail.com>
2022-08-08 16:53:41 +0530

    js-array-practice

---
+++ array-lastindex/script.js
@@ -0,0 +1,17 @@
+//last indexof value
+//String
+
+let a=["bus","car","bus","train","bus"];
+
+console.log(a);
+
+let b=console.log(a.lastIndexOf("bus"))
+
+// Number
+
+let x=[10,20,10,20,10,30,10];
+
+console.log(x);
+
+let y=console.log(x.lastIndexOf(10));
+
